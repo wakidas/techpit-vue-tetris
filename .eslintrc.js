ef837ab0f90510edb1b1f5a01fd5ed118cd6d634
Shimpei Wakida <[shimpei.wakida@gmail.com>
2022-02-06 18:36:38 +0900

    Run linter

---
+++ .eslintrc.js
@@ -0,0 +1,13 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+  root: true,
+  extends: [
+    "eslint:recommended",
+    "plugin:vue/vue3-recommended",
+    "@vue/eslint-config-typescript/recommended"
+  ],
+  rules: {
+    "no-redeclare": "off"
+  }
+};
+
