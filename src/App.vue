0a9dff46c05b700c52dad08b4fd4977e3f8f9cb3
Shimpei Wakida <[shimpei.wakida@gmail.com>
2022-02-06 18:58:55 +0900

    TopPageを書き換え

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,9 @@
 <script setup lang="ts">
-// This starter template is using Vue 3 <script setup> SFCs
-// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
-</script>
-
+ import TopPage from "./pages/TopPage.vue";
+ </script>
+ 
 <template>
-  <img
-    alt="Vue logo"
-    src="./assets/logo.png"
-  >
+  <TopPage />
 </template>
 
 <style>
